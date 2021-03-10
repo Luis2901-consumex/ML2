8886b88fdd02d56b83bc28f81b0c24bb2c67ccf3
Luis2901-consumex <80370117+Luis2901-consumex@users.noreply.github.com>
2021-03-10 12:22:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -37,25 +37,72 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/posts', validateToken, async (req, res) => {
+app.post('/login', (req, res) => {
+  if (req.body.password === SYS_PWD) {
+    req.session.user = true;
+    res.redirect('/home');
+  } else {
+    res.redirect('/?error=senha-incorreta');
+  }
+});
+
+app.get('/home', validateToken, async (req, res) => {
   try {
-    const meliObject = new MeliObject(res.locals.access_token);    
+    const meliObject = new MeliObject(res.locals.access_token);
     const user = await meliObject.get('/users/me');
-    const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
-    if (items.length) {
-      const result = [];
-      const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
-      for await (item of promises) {
-        result.push(item);
-      }
-      res.render('posts', { items: result });
-    } else {
-      res.status(404).send('no items were found :(');
-    }
+    const categories = await meliObject.get(`/sites/${user.site_id}/categories`);
+    const currencies = await meliObject.get('/currencies');
+    const listing_types = await meliObject.get(`/sites/${user.site_id}/listing_types`);
+    res.render('home', {
+      user,
+      categories,
+      currencies,
+      listing_types
+    });
+  } catch (err) {
+    console.log('Something went wrong', err);
+    res.status(500).send(`Error! ${err}`);
+  }
+});
+
+app.post('/post', validateToken, upload.single('picture'), async (req, res) => {
+  try {
+    const meliObject = new MeliObject(res.locals.access_token);
+    const user = await meliObject.get('/users/me');
+    const predict = await meliObject.get(`/sites/${user.site_id}/category_predictor/predict?title=${encodeURIComponent(req.body.title)}`);
+    await meliObject.post('/items', {
+      title: req.body.title,
+      category_id: predict.id,
+      price: req.body.price,
+      currency_id: req.body.currency,
+      available_quantity: req.body.quantity,
+      buying_mode: 'buy_it_now',
+      listing_type_id: req.body.listing_type,
+      condition: req.body.condition,
+      description: req.body.description,
+      tags: [ 'immediate_payment' ],
+      pictures: [
+        {
+          source: `${req.protocol}://${req.get('host')}/pictures/${req.file.filename}`
+        }
+      ]
+    });
+    console.log('publicado na categoria:', predict.name);
+    console.log('category probability (0-1):', predict.prediction_probability, predict.variations);
+    res.redirect('/posts');
   } catch(err) {
     console.log('Something went wrong', err);
     res.status(500).send(`Error! ${err}`);
   }
+});
+
+// TODO: exemplo de notificações
+app.get('/notifications', (req, res) => {
+  res.send('ok');
+  console.log(req.body);
+  // Recomendamos enviar um status 200 o mais rápido possível.
+  // Você pode fazer algo assíncrono logo em seguida. Por exemplo
+  // salvar num banco de dados de tempo real, como o firebase.
 });
 
 module.exports = app;
