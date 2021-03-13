912cdb5dd15d646292b2a140d7164ae8e49d25dd
Luis2901-consumex <80370117+Luis2901-consumex@users.noreply.github.com>
2021-03-13 03:03:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
   if (req.body.password === SYS_PWD) {
     req.session.user = true;
-    alert("Ingreso");
     res.redirect('/home');
   } else {
     res.redirect('/?error=senha-incorreta');
